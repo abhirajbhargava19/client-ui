b4efbbfd741191eb2b5aa084c3f77a61b8b067d8
Abhiraj <abhirajbhargava19@email.com>
2025-11-18 13:27:42 +0530

    update header component

--- components/ui/custom/header.tsx
+++ components/ui/custom/header.tsx
@@ -8,7 +8,7 @@ import { Button } from '../button'
 const Header = () => {
   return (
     <header className='bg-white'>
-        <nav className='@container py-5 flex items-center justify-between'>
+        <nav className='@container mx-auto py-5 flex items-center justify-between'>
             <div className="flex items-center space-x-4">
                 <svg
     data-testid="logo"
